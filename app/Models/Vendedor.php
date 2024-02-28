5f7faceb543538d998e1550844bd6e2ed9ba5cf1
Anthonny <heatdkwow@gmail.com>
2024-02-28 15:30:36 -0500

    Vendedores

---
+++ app/Models/Vendedor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vendedor extends Model
+{
+    protected $fillable = [
+        'nombres', 'rol','porcentaje_ventas', 'user_id'
+    ]; 
+
+    public function contratos(){
+        return $this->hasMany(Contrato::class, 'vendedor_id');
+    }
+
+    use HasFactory;
+}

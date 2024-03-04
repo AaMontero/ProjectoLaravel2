432e0fcbb407af0b2aea7e102f8d33fbd831eb4b
Anthonny <heatdkwow@gmail.com>
2024-03-04 16:28:17 -0500

    ruta para Update Vendedor

--- resources/views/vendedor/index.blade.php
+++ resources/views/vendedor/index.blade.php
@@ -76,12 +76,12 @@
     </div>
     <div class="py-2 ">
         <div class="max-w mx-auto px-2 lg:px-20 mb-4">
-            <h2 class = "ml-8">Cliente Registrados</h2>
+            <h2 class = "ml-8">Vendedores Registrados</h2>
             <div class="bg-white dark:bg-gray-900 bg-opacity-50 shadow-lg rounded-lg ">
-                <div class="p-6 text-gray-900 dark:text-gray-100 ">
-                    <table class="w-100 bg-white dark:bg-gray-800 border border-gray-300">
+                <div class="p-6 text-gray-900 dark:text-gray-100 overflow-auto" >
+                    <table class="w-100 bg-white dark:bg-gray-800 border border-gray-300"  style="overflow-x: auto;">
                         <thead>
-                            <tr> <!--Etiquetas de la tabla de clientes-->
+                            <tr> 
                                 <th class="py-2 px-4 border-b text-center ">ID</th>
                                 <th class="py-2 px-4 border-b text-center ">Nombres</th>
                                 <th class="py-2 px-4 border-b text-center ">Rol</th>
@@ -91,7 +91,7 @@
                         </thead>
                         <tbody>
                             @foreach ($vendedores as $vendedor)
-                                <tr> <!--Tabla que muestra los clientes-->
+                                <tr> 
                                     <td class="py-2 px-4 border-b text-center">{{ $vendedor->id }}</td>
                                     <td class="py-2 px-4 border-b text-center">{{ $vendedor->nombres }}</td>
                                     <td class="py-2 px-4 border-b text-center">{{ $vendedor->rol }}</td>

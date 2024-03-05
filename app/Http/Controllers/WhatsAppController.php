60e0b6a9d1eaf922d6daf6b0cb65413079e61e36
Anthonny <heatdkwow@gmail.com>
2024-03-05 18:00:26 -0500

    comentario

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -1,105 +1,156 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\Notificacion;
 use App\Models\WhatsApp;
+use DateTime;
+use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 
 class WhatsAppController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $mensajes = WhatsApp::all(); // Por ejemplo, aquí obtienes todos los mensajes de tu modelo
         return view('chat.chat', ['mensajes' => $mensajes]);
+    }
 
+    public function enviarPHP(Request $request)
+    {
+        $mensaje = $request->mensajeEnvio; 
+        $numeroEnviar = $request->numeroEnvio; 
+        if(strncmp($numeroEnviar, '0', strlen('0')) === 0){
+            $numeroEnviar = '593' . substr($numeroEnviar, 1);
+        }
+        $telefonoEnviaID = "258780720641927";
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://graph.facebook.com/v18.0/' . $telefonoEnviaID . '/messages',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode([
+                "messaging_product" => "whatsapp",
+                "to" => $numeroEnviar, // Número de teléfono del destinatario
+                "type" => "text", // Tipo de mensaje
+                "text" => [
+                    "body" => $mensaje, // Cuerpo del mensaje de texto
+                ],
+            ]),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Bearer EAA0cGBz1VmwBOyPIAWVggBulUedn4Bxeq1VzHNHxZBW25Hh11bfuZBOz704eaq7MP06LgoRzKSuCaJFw6ZCWsA8pb6dqrqC3yaruT2hhopjoCF7mTO4nMyNG7XyQ5OcLPSujzHzAqcPksvlQLk3igDh2FnXyZA803qaiIdzJO1x09euof8c2XYBZBy9VNfcr3haXmD4HOVJByZBIlYLTQZD'
+            ),
+        ));
+
+
+        $response = curl_exec($curl);
+        $mensajeenv = new Whatsapp(); 
+        $mensajeenv->mensaje_recibido = $mensaje; 
+        $mensajeenv->id_wa = "asdasdasd"; 
+        $mensajeenv->telefono_wa = "593987411818"; 
+        $mensajeenv->id_numCliente = $numeroEnviar ; 
+        $mensajeenv->fechaHora = new DateTime('now'); 
+        $mensajeenv->create(); 
+
+
+        curl_close($curl);
+        echo $response;
     }
     public function envia(Request $request)
     {
 
-        $enviado = $request->input('enviado');
-        $telefonoCliente = $request->input('telefonoCliente');
-
+        $enviado = "Texto";
+        //$telefonoCliente = $request->input('telefonoCliente');
+        $telefonoCliente = '593979345051';
         //token que nos da facebook
-        $token ='EAA0cGBz1VmwBO2PfnWb1Ih05lj3PagvoDGM0JIZCMqKXmEZCnFd7Ntdws2d2IOfGpAYAWQAxO5F5tzrZBfXQdQPdQXzfZCsoqZC1RahWBZCQFm7clpzm38bpc50yKu4Oy9ZCK9egwTcasNdwqHZC5XdnGkBZAAzgK2H4kzWnz3Yqgg6l086YZCR5jHXoGtFgbiMoZCZCsl7KT7MzepVuGbjIMt0ZD';
+        $token = 'EAA0cGBz1VmwBO7JfpWGn8eR74d51Kk36ahQtEDTUo8nMow9Hytfmvpe2jCzWkQQrA1JzzDlN6aXen9myp9m75dYcLh7scSXdayhdmTwbjhMJzMb9UrgFqfFNzhxDSZCdPNtTSCq5y4gGS2Woi1gPHudZCp5jOivjJjAuiVY8qTwH50lQJIp6heMGp3yJpb';
         // nuestro telefono
-        $telefonoID = '224013397467233';
+        $telefonoID = '258780720641927';
         //url a donde se manda el mensaje
-        $url = 'https://graph.facebook.com/v15.0/' . $telefonoID . '/messages';
+        $url = 'https://graph.facebook.com/v18.0/258780720641927/messages';
 
-         //CONFIGURACION DEL MENSAJE
+        //CONFIGURACION DEL MENSAJE
         $mensaje = ''
-                . '{'
-                . '"messaging_product": "whatsapp", '
-                . '"recipient_type": "individual",'
-                . '"to": "' . $telefonoCliente . '", '
-                . '"type": "text", '
-                . '"text": '
-                . '{'
-                . '     "body":"' . $enviado . '",'
-                . '     "preview_url": true, '
-                . '} '
-                . '}';
+            . '{'
+            . '"messaging_product": "whatsapp", '
+            . '"recipient_type": "individual",'
+            . '"to": "' . $telefonoCliente . '", '
+            . '"type": "text", '
+            . '"text": '
+            . '{'
+            . '     "body":  "' . $enviado . '",'
+            . '     "preview_url": true, '
+            . '} '
+            . '}';
 
         //declaramos las cabeceras
-        $header = array("Authorization: Bearer ".$token, "Content-Type: application/json");
+        $header = array("Authorization: Bearer " . $token, "Content-Type: application/json");
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $mensaje);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        $response = json_decode(curl_exec($curl),true);
-
-        print_r($response);
-
+        $response = curl_exec($curl);
+        //file_put_contents("response.txt", $response);
         $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
         curl_close($curl);
-
-
-        return redirect()->route('chat.chat');
-
+        
+        $mensajes = WhatsApp::all(); // Por ejemplo, aquí obtienes todos los mensajes de tu modelo
+        return view('chat.chat', ['mensajes' => $mensajes]);
     }
-    public function webhook(){
-        //TOQUEN QUE QUERRAMOS PONER
-        $token = 'TokenValidacion';
-        //RETO QUE RECIBIREMOS DE FACEBOOK
-        $hub_challenge = isset($_GET['hub_challenge']) ? $_GET['hub_challenge'] : '';
-        //TOQUEN DE VERIFICACION QUE RECIBIREMOS DE FACEBOOK
-        $hub_verify_token = isset($_GET['hub_verify_token']) ? $_GET['hub_verify_token'] : '';
-        //SI EL TOKEN QUE GENERAMOS ES EL MISMO QUE NOS ENVIA FACEBOOK RETORNAMOS EL RETO PARA VALIDAR QUE SOMOS NOSOTROS
-        if ($token === $hub_verify_token) {
-            echo $hub_challenge;
-            exit;
+    public function webhook(Request $request)
+    {
+        
+        try {
+            $verifyToken = 'TokenPruebaValidacion';
+            $query = $request->query();
+            $mode = $query['hub_mode'];
+            $token = $query['hub_verify_token'];
+            $challenge = $query['hub_challenge'];
+            if ($mode && $token) {
+                if ($mode === 'subscribe' && $token == $verifyToken) {
+                    return response($challenge, 200)->header('Content-Type', 'text/plain');
+                }
+            }
+            throw new Exception('Invalid Request');
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'error' => $e->getMessage(),
+            ], 500);
         }
-      }
-      /*
+    }
+    /*
       * RECEPCION DE MENSAJES
       */
-    public function recibe(){
+    public function recibe()
+    {
         //LEEMOS LOS DATOS ENVIADOS POR WHATSAPP
         $respuesta = file_get_contents("php://input");
         //echo file_put_contents("text.txt", "Hola");
         //SI NO HAY DATOS NOS SALIMOS
-        if($respuesta==null){
-          exit;
+        if ($respuesta == null) {
+            exit;
         }
         //CONVERTIMOS EL JSON EN ARRAY DE PHP
         $respuesta = json_decode($respuesta, true);
         //EXTRAEMOS EL TELEFONO DEL ARRAY
-        $mensaje="Telefono:".$respuesta['entry'][0]['changes'][0]['value']['messages'][0]['from']."\n";
+        $mensaje = "Telefono:" . $respuesta['entry'][0]['changes'][0]['value']['messages'][0]['from'] . "\n";
         //EXTRAEMOS EL MENSAJE DEL ARRAY
-        $mensaje.="Mensaje:".$respuesta['entry'][0]['changes'][0]['value']['messages'][0]['text']['body'];
+        $mensaje .= "Mensaje:" . $respuesta['entry'][0]['changes'][0]['value']['messages'][0]['text']['body'];
         //GUARDAMOS EL MENSAJE Y LA RESPUESTA EN EL ARCHIVO text.txt
 
     }
 
-    public function notificacionMensaje(){
+    public function notificacionMensaje()
+    {
         $mensajes = WhatsApp::all();
-
-
-
         return view('chat.chat', compact('mensajes')); // Pasa los mensajes a la vista 'dashboard'
     }
 
--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
     <x-slot name="header">
+        <link rel="shortcut icon" href="#">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 {{ __('Clients') }}

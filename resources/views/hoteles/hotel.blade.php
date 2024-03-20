02b4a2cbed51b741627f6fea49f20c0ef98f8cab
Erick <smkerick@gmail.com>
2024-03-20 16:15:36 -0500

    hoteles-lista

--- resources/views/hoteles/hotel.blade.php
+++ resources/views/hoteles/hotel.blade.php
@@ -6,26 +6,26 @@
         <div class="flex justify-between items-center">
             <div x-data="{ showModal: false }" x-cloak class="flex items-center gap-5">
                 @role('superAdmin')
-                    <x-nav-link :href="route('paquetes.paquetes')"
-                        class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight no-underline">
-                        {{ __('Paquetes') }}
-                    </x-nav-link>
-                    {{-- hoteles --}}
-                    <x-nav-link href="{{ route('hotel') }}"
-                        class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight no-underline">
-                        {{ __('Hoteles') }}
-                    </x-nav-link>
+                <x-nav-link :href="route('paquetes.paquetes')"
+                    class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight no-underline">
+                    {{ __('Paquetes') }}
+                </x-nav-link>
+                {{-- hoteles --}}
+                <x-nav-link href="{{ route('hotel') }}"
+                    class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight no-underline">
+                    {{ __('Hoteles') }}
+                </x-nav-link>
                 @endrole
             </div>
 
             @role('Administrador|superAdmin')
-                <div onclick="abrirVentanaAgregarPaquete()" class="cursor-pointer flex items-center">
-                    <span class="mr-2">Agregar un nuevo hotel</span>
-                    <svg class="h-6 w-6 text-gray-600 dark:text-gray-400" fill="none" stroke="currentColor"
-                        stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7"></path>
-                    </svg>
-                </div>
+            <div onclick="abrirVentanaAgregarPaquete()" class="cursor-pointer flex items-center">
+                <span class="mr-2">Agregar un nuevo hotel</span>
+                <svg class="h-6 w-6 text-gray-600 dark:text-gray-400" fill="none" stroke="currentColor" stroke-width="2"
+                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
+            </div>
             @endrole
         </div>
     </x-slot>
@@ -35,15 +35,15 @@
             style="{{ $errors->any() ? 'display: block;' : 'display: none;' }}">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form method="POST" action = "{{ route('hoteles.store') }} " enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('hoteles.store') }} " enctype="multipart/form-data">
                         @csrf
                         <label class="mt-0.5 font-bold p-0 ml-4" for="hotel_nombre">Nombre hotel:</label>
                         <input type="text" name="hotel_nombre" id="hotel_nombre"
                             class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             placeholder="{{ __('Ingresa el nombre del hotel') }}" value="{{ old('hotel_nombre') }}">
                         @error('hotel_nombre')
-                            <small class = "text-red-500 ml-2">{{ $message }}</small>
-                            <br>
+                        <small class="text-red-500 ml-2">{{ $message }}</small>
+                        <br>
                         @enderror
                         <div class="flex w-full">
                             <div class="mr-2 flex-1">
@@ -66,39 +66,51 @@
                             </div>
                         </div>
 
-                       
+
                         <label class="mt-3 font-bold p-0 ml-4" for="num_h">Número de habitaciones:</label>
                         <input type="number" name="num_h" id="num_h"
                             class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             placeholder="{{ __('Ingresa el número de habitaciones') }}" value="{{ old('num_h') }}">
                         @error('num_h')
-                            <small class = "text-red-500 ml-2">{{ $message }}</small>
-                            <br>
+                        <small class="text-red-500 ml-2">{{ $message }}</small>
+                        <br>
                         @enderror
                         <label class="mt-3 font-bold p-0 ml-4" for="num_camas">Número de camas:</label>
                         <input type="number" name="num_camas" id="num_camas"
                             class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             placeholder="{{ __('Ingresa el número de camas') }}" value="{{ old('num_camas') }}">
                         @error('num_camas')
-                            <small class = "text-red-500 ml-2">{{ $message }}</small>
-                            <br>
+                        <small class="text-red-500 ml-2">{{ $message }}</small>
+                        <br>
                         @enderror
                         <label class="mt-3 font-bold p-0 ml-4" for="precio">Precio:</label>
                         <input type="number" name="precio" id="precio" step="0.01"
                             class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             placeholder="{{ __('Ingresa el precio') }}" value="{{ old('precio') }}">
                         @error('precio')
-                            <small class = "text-red-500 ml-2">{{ $message }}</small>
-                            <br>
+                        <small class="text-red-500 ml-2">{{ $message }}</small>
+                        <br>
                         @enderror
-                        <label class="mt-3 font-bold p-0 ml-4" for="servicios">Servicios:</label>
-                        <input type="text" name="servicios" id="servicios"
-                            class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                            placeholder="{{ __('Ingresa los servicos') }}"
-                            value="{{ old('servicios') }}">
+                        <div>
+                            <label class="mt-3 font-bold p-0 ml-4" for="servicios[]">Servicios:</label>
+                            <input type="text" name="servicios[]"
+                                class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                placeholder="{{ __('Ingresa un servicio') }}">
+                        </div>
+
+                            <div id="servicios-container" class="m-3">
+                                <!-- nuevo input de opinion -->
+                            </div>
+    
+                            <div>
+                                <button type="button" id="agregar-servicio-btn" class="mt-1 bg-blue-950 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
+                                    Agregar otra servicio
+                                  </button>
+                                  
+                            </div>
                         @error('servicios')
-                            <small class = "text-red-500 ml-2">{{ $message }}</small>
-                            <br>
+                        <small class="text-red-500 ml-2">{{ $message }}</small>
+                        <br>
                         @enderror
                         <label class="mt-3 font-bold p-0 ml-4" for="tipo_alojamiento">Tipo de alojamiento:</label>
                         <select name="tipo_alojamiento" id="tipo_alojamiento" class="form-control mb-2">
@@ -108,27 +120,39 @@
                         </select>
 
                         @error('tipo_alojamiento')
-                            <small class = "text-red-500 ml-2">{{ $message }}</small>
-                            <br>
+                        <small class="text-red-500 ml-2">{{ $message }}</small>
+                        <br>
                         @enderror
-                        <label class="mt-3 font-bold p-0 ml-4" for="opiniones">Opiniones:</label>
-                        <input type="text" name="opiniones" id="opiniones"
-                            class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                            placeholder="{{ __('Ingresa las opiniones') }}"
-                            value="{{ old('opiniones') }}">
+                        <div>
+                            <label class="mt-3 font-bold p-0 ml-4" for="opiniones[]">Opiniones:</label>
+                            <input type="text" name="opiniones[]"
+                                class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                placeholder="{{ __('Ingresa una opinión') }}">
+                        </div>
+
+                        <div id="opiniones-container" class="m-3">
+                            <!-- nuevo input de opinion -->
+                        </div>
+
+                        <div>
+                            <button type="button" id="agregar-opinion-btn" class="mt-1 bg-blue-950 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
+                                Agregar otra opinión
+                              </button>
+                              
+                        </div>
+
                         @error('opiniones')
-                            <small class = "text-red-500 ml-2">{{ $message }}</small>
-                            <br>
+                        <small class="text-red-500 ml-2">{{ $message }}</small>
+                        <br>
                         @enderror
                         <label class="mt-3 font-bold p-0 ml-4" for="imagen_hotel">Imágenes del hotel:</label>
-                        <input type="file" name="imagen_hotel[]" id="imagen_hotel" class="form-control mb-2"
-                            multiple>
+                        <input type="file" name="imagen_hotel[]" id="imagen_hotel" class="form-control mb-2" multiple>
                         <div id="preview-container" class="flex flex-wrap">
                             <!-- Aquí se mostrarán las imágenes -->
                         </div>
                         @error('imagen_hotel')
-                            <small class = "text-red-500 ml-2">{{ $message }}</small>
-                            <br>
+                        <small class="text-red-500 ml-2">{{ $message }}</small>
+                        <br>
                         @enderror
                         <x-primary-button class='mt-4'>Agregar nuevo hotel</x-primary-button>
                     </form>
@@ -136,6 +160,39 @@
             </div>
         </div>
         <script>
+        //agregar un input dinamico en las opiniones
+        document.addEventListener("DOMContentLoaded", function() {
+        var opinionesContainer = document.getElementById('opiniones-container');
+        var agregarOpinionBtn = document.getElementById('agregar-opinion-btn');
+
+        agregarOpinionBtn.addEventListener('click', function() {
+            var nuevaOpinionInput = document.createElement('input');
+            nuevaOpinionInput.type = 'text';
+            nuevaOpinionInput.name = 'opiniones[]';
+            nuevaOpinionInput.className = 'mt-3 block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50';
+            nuevaOpinionInput.placeholder = 'Ingresa otra opinión';
+
+            // Insertar el nuevo campo antes del botón "Agregar otra opinión"
+            agregarOpinionBtn.parentNode.insertBefore(nuevaOpinionInput, agregarOpinionBtn);
+            });
+        });
+
+        //agregar un input dinamico en los servicios
+        document.addEventListener("DOMContentLoaded", function() {
+        var opinionesContainer = document.getElementById('servicios-container');
+        var agregarOpinionBtn = document.getElementById('agregar-servicio-btn');
+
+        agregarOpinionBtn.addEventListener('click', function() {
+            var nuevaOpinionInput = document.createElement('input');
+            nuevaOpinionInput.type = 'text';
+            nuevaOpinionInput.name = 'servicios[]';
+            nuevaOpinionInput.className = 'mt-3 block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50';
+            nuevaOpinionInput.placeholder = 'Ingresa otro servicio';
+
+            // Insertar el nuevo campo antes del botón "Agregar otra opinión"
+            agregarOpinionBtn.parentNode.insertBefore(nuevaOpinionInput, agregarOpinionBtn);
+            });
+        });
             //previsializar las imagenes antes de subirlas
             document.getElementById('imagen_hotel').addEventListener('change', function(e) {
                 var files = e.target.files;

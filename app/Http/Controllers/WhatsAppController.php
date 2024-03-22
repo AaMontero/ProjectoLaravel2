4ef47e3766f4a6bd816057bf9516b6e6b6ca1560
Joho06 <jhjaviersartica@gmail.com>
2024-03-22 12:04:25 -0500

    chat quemado corregido 1

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -280,34 +280,38 @@ class WhatsAppController extends Controller
         $util = new Utils();
 
         $mensajenoTilde = $util->convertirMinNoTilde($mensajeRecibido);
-        switch ($mensajenoTilde) {case $util->convertirMinNoTilde("¡Hola! Me gustaría obtener más información sobre sus servicios de viaje. ¿Podrían proporcionarme detalles sobre los destinos, paquetes disponibles y precios?"):
-                return  "¡Hola! Claro, aquí tienes tres opciones de paquetes de viaje:\nDestino: Cancún, México\nPaquete Todo Incluido: 7 días y 6 noches en una habitación doble.Precio: $1500 por persona.Servicios: Playa privada, acceso ilimitado al spa y actividades acuáticas.\n
-                Destino: París, Francia
-                Paquete Romántico: 5 días y 4 noches en una suite.
-                Precio: $2500 por persona.
-                Servicios: Tour privado por la ciudad, cena romántica en la Torre Eiffel y paseo en barco por el Sena.\n
-                Destino: Tokio, Japón
-                Paquete Aventura: 10 días y 9 noches en una habitación individual.
-                Precio: $3000 por persona.
-                Servicios: Tour por los templos, experiencia en el distrito de Akihabara y clases de sushi.\n
-                Por favor, selecciona el número del paquete que te interese o si necesitas más detalles sobre alguno de ellos.";
+        switch ($mensajenoTilde) {
+            case $util->convertirMinNoTilde("¡Hola! Me gustaría obtener más información sobre sus servicios de viaje. ¿Podrían proporcionarme detalles sobre los destinos, paquetes disponibles y precios?"):
+                return "¡Encantado! Ayúdame con su nombre y su número de cédula.";
                 break;
-            case $util->convertirMinNoTilde("Me interesa el paquete número 2, a París"):
-                return "Perfecto, ¿cuántas personas viajarán contigo?";
+            case $util->convertirMinNoTilde("Paul, mi número de cédula es: 1234567890"):
+                return "Tras verificar en la base de datos, no se encuentra registrado.\nPara agilizar el proceso, le recomendamos registrarse.\n¿Sobre qué servicio quisieras tener información?";
                 break;
-            case $util->convertirMinNoTilde("Voy con mi pareja, plan romantico"):
-                return "Excelente, ¿en qué fechas te gustaría reservar tu viaje a París?";
+            case $util->convertirMinNoTilde("No estoy seguro del destino, ¿qué paquetes me recomiendas?"):
+                return  "Claro, aquí tienes tres opciones de paquetes de viaje:\nDestino: Cancún, México\nPaquete Todo Incluido: 7 días y 6 noches en una habitación doble.\nPrecio: $1500 por persona.\nServicios: Playa privada, acceso ilimitado al spa y actividades acuáticas.\n\nDestino: París, Francia\nPaquete Romántico: 5 días y 4 noches en una suite.\nPrecio: $2500 por persona.\nServicios: Tour privado por la ciudad, cena romántica en la Torre Eiffel y paseo en barco por el Sena.\n\nDestino: Tokio, Japón.\nPaquete Aventura: 10 días y 9 noches en una habitación individual.\nPrecio: $3000 por persona.\nServicios: Tour por los templos, experiencia en el distrito de Akihabara y clases de sushi.\n\nPor favor, selecciona el paquete que te interese o si necesitas más detalles sobre alguno de ellos.";
                 break;
-            case $util->convertirMinNoTilde("Voy con mi pareja, plan romantico"):
-                return "Excelente, ¿en qué fechas te gustaría reservar tu viaje a París?";
+            case $util->convertirMinNoTilde("No estoy interesado por un paquete internacional, vi en la pagina un paquete en Diamond Beach"):
+                return"Claro, aquí tienes los detalles del paquete Diamond Beach:\nHotel: Diamond Beach Resort & Spa.\nPrecio: $200 por habitacion.\nHabitaciones: Habitación doble con vista al mar.\nServicios: Acceso exclusivo a la playa privada, spa de lujo y actividades acuáticas.\n¿Te gustaría reservar este paquete o necesitas más información?";
                 break;
-            case $util->convertirMinNoTilde("Del 15 al 19 de mayo"):
-                return "Hecho, tu reserva para el paquete Romántico a París del 15 al 19 de mayo para dos personas ha sido confirmada. Por favor, proporciona tu información de contacto para completar la reserva.";
+            case $util->convertirMinNoTilde("Cuáles son las fechas disponibles para este paquete?"):
+                return "Las fechas disponibles para el paquete Diamond Beach son del 10 al 20 de agosto, del 20 al 31 de septiembre y del 5 al 15 de octubre. ¿Te gustaría reservar?";
                 break;
-            case $util->convertirMinNoTilde("Paul Alexander Romero, paulromero90@gmail.com, 0998557785"):
-                return "¡Gracias! Tu reserva está confirmada. Para finalizar el proceso, por favor haz clic en el siguiente enlace para proceder con el pago: https://www.payphone.app/,\n\nUna vez completado, recibirás un correo electrónico con todos los detalles de tu viaje. ¿Hay algo más en lo que pueda ayudarte?";
+            case $util->convertirMinNoTilde("Me gustaría reservar para el 10 al 15 de agosto"):
+                return "Perfecto, ¿cuántas personas serán?";
                 break;
-            case $util->convertirMinNoTilde("Eso seria todo, Gracias"):
+            case $util->convertirMinNoTilde("Dos personas"):
+                return "Entendido, tu reserva para el paquete Diamond Beach del 10 al 15 de agosto para dos personas ha sido confirmada. Por favor, proporcioname tu nombre completo, un correo electronico y tu numero de telefono.";
+                break;
+            case $util->convertirMinNoTilde("Paul Alexander Romero Chiliguisa, paulromero90@gmail.com, 0998557785"):
+                return "¡Gracias!, tenemos estas opciones de pago:\n1-Transferencia\n2-Pago con tarjeta(PayPhone)\no puedes solicitar contactarte con uno de nuestros Asesores especializados.";
+                break;
+            case  $util->convertirMinNoTilde("Voy a realizar mi pago con una tarjeta de credito"):
+                return"Para finalizar el proceso, por favor haz clic en el siguiente enlace para proceder con el pago: https://www.payphone.app/.\n\nUna vez completado, recibirás un correo electrónico con todos los detalles de tu viaje.";
+                break;
+            case $util->convertirMinNoTilde("En este momento ya realice el pago"):
+                return"En este momento estamos verificando el pago...\n\nTu pago se ha creditado correctamente.\n ¿Hay algo más en lo que pueda ayudarte?";
+                break;
+            case $util->convertirMinNoTilde("Eso seria todo por el momento, Gracias"):
                 return "Gracias a ti, Buen viaje";
                 break;
             default:

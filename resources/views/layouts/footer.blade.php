108e781929ce26a23df59c64dbae2d7e744abaa7
Erick <smkerick@gmail.com>
2024-03-25 09:46:20 -0500

    the snausers

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,6 @@
+<!-- Agrega SweetAlert y su CSS a tu página -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
 <footer class="footer py-4">
     <div class="container-fluid">
         <div class="row align-items-center justify-content-lg-between">
@@ -11,7 +14,7 @@
                         document.write(new Date().getFullYear())
                     </script>,
                     made with <i class="fa fa-heart"></i> by
-                    <a href="#" class="font-weight-bold" target="_blank">The Snausers</a>
+                    <a href="#" class="font-weight-bold"  id="openModal" target="_blank">The Snausers</a>
                     for a better web.
 
                 </div>
@@ -19,4 +22,35 @@
 
         </div>
     </div>
+    <script>
+        // Agrega un evento de clic al enlace
+        document.getElementById('openModal').addEventListener('click', function(event) {
+          // Evita el comportamiento predeterminado del enlace
+          event.preventDefault();
+      
+          // Muestra el modal de SweetAlert
+          Swal.fire({
+            title: "Creado por los Snausers",
+            width: 600,
+            padding: "3em",
+            color: "#716add",
+            background: "#fff url(https://sweetalert2.github.io/images/trees.png)",
+            backdrop: `
+              rgba(0,0,123,0.4)
+              url(https://sweetalert2.github.io/images/nyan-cat.gif)
+              left top
+              no-repeat
+            `,
+
+          }).then((result) => {
+            if (result.isConfirmed) {
+              // Aquí puedes hacer algo si el usuario confirma
+              console.log('El usuario confirmó');
+            } else if (result.isDenied) {
+              // Aquí puedes hacer algo si el usuario niega
+              console.log('El usuario negó');
+            }
+          });
+        });
+      </script>
 </footer>

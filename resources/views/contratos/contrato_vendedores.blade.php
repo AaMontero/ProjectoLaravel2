048c749599886aa667d3cbc0988713366a6f4e17
Anthonny <heatdkwow@gmail.com>
2024-04-01 17:11:05 -0500

    urlSegundaOpcion

--- resources/views/contratos/contrato_vendedores.blade.php
+++ resources/views/contratos/contrato_vendedores.blade.php
@@ -9,6 +9,8 @@
     </x-slot>
     <div class="py-2">
         <a href="{{ $ruta }}" download>Descargar Documento</a>
+        <a href="{{ url($ruta) }}" download>Descargar Documento</a>
+
         <div id="" class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-4" style=""> <!--"-->
             <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">

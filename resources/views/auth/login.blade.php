fda19f682c857ee5d8f06dafede0912d361d023a
Joho06 <jhjaviersartica@gmail.com>
2024-04-05 09:17:08 -0500

    revision

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    {{-- <x-auth-session-status class="mb-4" :status="session('status')" /> --}}
 
     <form method="POST" action="{{ route('login') }}">
         @csrf

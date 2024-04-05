2f0500bd9ebc5ffff9f3d441368685b7b2d664bc
Joho06 <jhjaviersartica@gmail.com>
2024-04-05 13:04:52 -0500

    calendario horas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,28 +7,37 @@
         </h2>
     </x-slot>
     <style>
-        .imagen{
-        background-image: url("{{ asset('images/travel.jpg') }}");
-        height: 650px;
-        width: 1400px;
-        margin-left: auto;
-        margin-right: auto;
-        background-size: cover; /* Ajusta la imagen para cubrir completamente el contenedor */
-        background-position: center; /* Centra la imagen dentro del contenedor */
-        border-radius: 10px;
+        .imagen-container {
+            position: relative;
+            width: 100%;
+            height: auto; /* Ajusta automáticamente la altura según el contenido */
+        }
+
+        .imagen {
+            width: 100%;
+            height: auto; /* Ajusta automáticamente la altura según el contenido */
+            border-radius: 10px;
+        }
+
+        .texto {
+            position: absolute;
+            top: 40%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
         }
     </style>
 
-<div class="mt-4">
-    <div class="imagen">
-        <div class="flex justify-center items-center h-96 mb-4">
-            <div class="text-center font-extrabold text-5xl">
+    <div class="mt-4">
+        <div class="imagen-container">
+            <img src="{{ asset('images/travel.jpg') }}" alt="Imagen" class="imagen">
+            <div class="texto">
                 <p class="bg-gradient-to-r from-blue-500 via-green-600 to-yellow-400 bg-clip-text text-transparent font-bold mb-4 text-xl md:text-4xl lg:text-5xl xl:text-6xl">BIENVENIDOS</p>
                 <p class="bg-gradient-to-r from-red-500 via-purple-600 to-pink-400 bg-clip-text text-transparent font-bold text-xl md:text-4xl lg:text-5xl xl:text-6xl">CRM de Qori Travel</p>
             </div>
         </div>
     </div>
-</div>
+
 </x-app-layout>
 @include('layouts.footer')
 

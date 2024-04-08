e342e507e34076844607f2ae7d20518372abbcce
Joho06 <jhjaviersartica@gmail.com>
2024-04-08 11:25:37 -0500

    rutase

--- routes/web.php
+++ routes/web.php
@@ -39,8 +39,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/', function () {
-        return view('welcome');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
     })->name('dashboard');
 
     Route::middleware('checkRole:Administrador,superAdmin')->group(function () {
